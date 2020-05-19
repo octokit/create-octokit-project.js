e6103414945d6cb31c92e5417394dd8a87a6f01b
Gregor Martynus <gregor@martynus.net>
2020-05-19 11:30:55 -0700

    fix: remove todo to install greenkeeper. Dependabot app will be installed globally and enabled automagically when creating the config file

--- create-octokit-project.js
+++ create-octokit-project.js
@@ -129,7 +129,6 @@ async function main() {
       title: "🚧 Initial version",
       body: `- [ ] Implement features. Create separate \`feat: ...\` commits for each feature of the initial version
 - [ ] 100% test coverage
-- [ ] Install https://github.com/apps/greenkeeper
 - [ ] Install https://github.com/apps/pika-ci
 - [ ] Create npm token at \`https://www.npmjs.com/settings/<your npm username>/tokens/create\` (with "Read and Publish" selected) and add it as \`NPM_TOKEN\` at Then create secret at https://github.com/${answers.repository}/settings/secrets
 `,
